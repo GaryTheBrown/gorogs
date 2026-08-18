aa737019f141b17f8ad99f37881e1d91d1f8d0ef
Gary The Brown <garybrownsunderland@gmail.com>
2026-08-18 20:08:52 +0100

    refactor of code

--- systems/helpers.go
+++ systems/helpers.go
@@ -7,10 +7,18 @@ import (
 
 func ShouldItStart(e SystemNameEnum) bool {
 	systemName := systemList[e].Name()
-	logger.DebugF(logName, "Is It [%s] Starting, Autostart: [%t] Disabled: [%t] Enabled: [%t]",
-		systemName, systemList[e].AutoStart(), config.IsDisabled(systemName), config.IsEnabled(systemName))
-	return (systemList[e].AutoStart() && !config.IsDisabled(systemName)) ||
-		(!systemList[e].AutoStart() && config.IsEnabled(systemName))
+
+	autoStart := systemList[e].AutoStart()
+	isDisabled := config.IsDisabled(systemName)
+	isEnabled := config.IsEnabled(systemName)
+
+	logger.InfoF(logName, "Evaluating service [%s] -> AutoStart: %t, Explicitly Disabled: %t, Explicitly Enabled: %t",
+		systemName, autoStart, isDisabled, isEnabled)
+
+	if autoStart {
+		return !isDisabled
+	}
+	return isEnabled
 }
 
 func SystemNameList() []string {
